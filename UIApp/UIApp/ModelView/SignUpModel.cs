7df21384b06d5a94024656c0b77750408ea98a4c
Mario A. <mac132@live.com.mx>
2023-10-04 17:59:00 -0600

    [Add] Project

---
+++ UIApp/UIApp/ModelView/SignUpModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace UIApp.ModelView
+{
+    class SignUpModel
+    {
+        public ICommand back => new Command(() => Application.Current.MainPage.Navigation.PopAsync());
+    }
+}
